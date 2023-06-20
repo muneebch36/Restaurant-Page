b8d74d3ca9779976bdd5bbccf2720c4b1afe823a
yomunnib <muneeb.cheema36@gmail.com>
2023-06-20 03:47:48 -0400

    Add makeHome, resetContent and createMenuItem functions.

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-alert("test");
+(()=>{function e(){const e=document.createElement("div"),n=document.createElement("img");return n.src="../src/nut.png",n.alt="nut",e.appendChild(n),e}e(),document.getElementById("content").appendChild(e())})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-alert("test");
+function makeHome() {
+    const home = document.createElement("div");
+
+    const homeImage = document.createElement("img");
+    homeImage.src = "../src/nut.png";
+    homeImage.alt = "nut";
+
+    home.appendChild(homeImage);
+
+return home;
+};
+
+function resetContent() {
+    const content = document.getElementById("content");
+    content.textContent = "";
+    content.appendChild(makeHome());
+}
+
+
+makeHome();
+resetContent();
+
+function createMenuItem(name,description) {
+    const menuItem = document.createElement("div");
+
+    const foodName = document.createElement("name");
+    foodName.textContent = name;
+
+    const foodDescription = document.createElement("p");
+    foodDescription.textContent = description;
+
+    menuItem.appendChild(foodName);
+    menuItem.appendChild(foodDescription);
+
+    return menuItem;
+}
